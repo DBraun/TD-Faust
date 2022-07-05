e465c1a086543ba473d1df8424f64d34887b84c0
David Braun <2096055+DBraun@users.noreply.github.com>
2022-07-05 08:28:45 -0700

    update faust

--- FaustLib.cmake
+++ FaustLib.cmake
@@ -43,12 +43,13 @@ set(INCLUDE_OSC         OFF CACHE STRING  "Include Faust OSC library"     FORCE)
 set(INCLUDE_HTTP        OFF CACHE STRING  "Include Faust HTTPD library"   FORCE)
 
 ## Hardcoded backends for faust
-## NB: The CPP_BACKEND is necessary for exporting a DSP to XML. 
+## NB: The CPP_BACKEND is necessary for exporting a DSP to XML and/or C++
+##     The INTERP_BACKEND is for using `createInterpreterDSPFactoryFromSignals` and `createInterpreterDSPFactoryFromBoxes`
 set(ASMJS_BACKEND  OFF                       CACHE STRING  "Include ASMJS backend"       FORCE)
 set(C_BACKEND      OFF                       CACHE STRING  "Include C backend"           FORCE)
 set(CPP_BACKEND    DYNAMIC                   CACHE STRING  "Include CPP backend"         FORCE)
 set(FIR_BACKEND    OFF                       CACHE STRING  "Include FIR backend"         FORCE)
-set(INTERP_BACKEND OFF                       CACHE STRING  "Include INTERPRETER backend" FORCE)
+set(INTERP_BACKEND DYNAMIC                   CACHE STRING  "Include INTERPRETER backend" FORCE)
 set(JAVA_BACKEND   OFF                       CACHE STRING  "Include JAVA backend"        FORCE)
 set(JS_BACKEND     OFF                       CACHE STRING  "Include JAVASCRIPT backend"  FORCE)
 set(LLVM_BACKEND   COMPILER DYNAMIC          CACHE STRING  "Include LLVM backend"        FORCE)
